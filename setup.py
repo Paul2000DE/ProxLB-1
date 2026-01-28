461d6b4df11bdd74cb4aa4e7c09228d3eece97fd
Florian Paul Azim Hoberg <florian.hoberg@credativ.de>
2026-01-28 15:07:13 +0100

    release(1.1.12): Create release 1.1.12 and set new maintaincer to credativ GmbH
    Fixes: #26

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="proxlb",
-    version="1.1.11",
+    version="1.1.12",
     description="An advanced resource scheduler and load balancer for Proxmox clusters.",
     long_description="An advanced resource scheduler and load balancer for Proxmox clusters that also supports maintenance modes and affinity/anti-affinity rules.",
     author="Florian Paul Azim Hoberg",
     author_email="gyptazy@gyptazy.com",
-    maintainer="Florian Paul Azim Hoberg",
-    maintainer_email="gyptazy@gyptazy.com",
+    maintainer="credativ GmbH",
+    maintainer_email="support@credativ.de",
     url="https://github.com/gyptazy/ProxLB",
     packages=["proxlb", "proxlb.utils", "proxlb.models"],
     install_requires=[
